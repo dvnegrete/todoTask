18752081e57bf7dde643f8b2db3da3bff6ada6dd
Damian Valenzuela Negrete <dvnegrete@gmail.com>
2021-11-23 22:33:57 -0600

    UseContext a Search y al Counter para el paso de los estados

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -8,36 +8,34 @@ import { CreateTodoButton } from "../CreateTodoButton";
 
 
 function AppUI () {
+  const {
+    error,
+    loading,
+    searchedTodos,
+    completedElement,
+    deletedElement
+  } = React.useContext(TodoContext);
+
     return(
     <React.Fragment>
       <TodoCounter />
       <TodoSearch/>
-      
-      <TodoContext.Consumer>
-        {({
-          error,
-          loading,
-          searchedTodos,
-          completedElement,
-          deletedElement
-        }) => (
-          <TodoList>
-          {error && <p>Tuvimos un error al carga tu lista de tareas :(</p>}
-          {loading && <p>Espera un poco, estamos cargado tus tareas...</p>}
-          {(!loading && !searchedTodos.length) && <p>¡Crear tu primera Tarea...!</p>}
-          {searchedTodos.map(todo => (
-            <TodoItem 
-            key ={todo.text}
-            text ={todo.text}
-            completed = {todo.completed}
-            onComplete = {()=>completedElement(todo.text)}
-            onDeleted = {()=> deletedElement(todo.text)}
-            />
-          ))}
+
+        <TodoList>
+        {error && <p>Tuvimos un error al carga tu lista de tareas :(</p>}
+        {loading && <p>Espera un poco, estamos cargado tus tareas...</p>}
+        {(!loading && !searchedTodos.length) && <p>¡Crear tu primera Tarea...!</p>}
+        {searchedTodos.map(todo => (
+          <TodoItem 
+          key ={todo.text}
+          text ={todo.text}
+          completed = {todo.completed}
+          onComplete = {()=>completedElement(todo.text)}
+          onDeleted = {()=> deletedElement(todo.text)}
+          />
+        ))}
         </TodoList>
-        )}
-      </TodoContext.Consumer>
-      
+        
       <CreateTodoButton/>
       
     </React.Fragment>
